1a772e55277ef46cd541c95492f4216523d136e5
Camille Hall <camille.hall@senseye.co>
2021-04-15 15:29:07 -0500

    updated UI and content

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,30 +4,40 @@ import {
   Image,
   StyleSheet,
   Dimensions,
-  ScrollView,
+  View,
   SafeAreaView,
   Text,
 } from 'react-native';
-
-import { SenseyeButton } from '@senseyeinc/react-native-senseye-sdk';
 
 const WINDOW_WIDTH = Dimensions.get('window').width;
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={require('../assets/senseye-white-logo.png')}
-      />
-      <ScrollView
-        style={styles.innerContainer}
-        contentContainerStyle={styles.layout}
-      >
-        <Text style={styles.warning}>{'hi'}</Text>
-
-        <SenseyeButton title="Submit" type={'primaryCta'} />
-      </ScrollView>
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require('../assets/senseye-white-logo.png')}
+        />
+        <Text style={[styles.text, styles.h3]}>{'ORM Check'}</Text>
+      </View>
+      <View style={styles.bodyContainer}>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>
+            {'This application uses the front facing camera'}
+          </Text>
+          <Text style={styles.text}>
+            {
+              'For optimal data collection the app will rotate on the next screen'
+            }
+          </Text>
+          <Text style={styles.text}>{'Please rotate phone accordingly'}</Text>
+        </View>
+        <Image
+          style={styles.arrow}
+          source={require('../assets/forward-arrow.png')}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -38,20 +48,24 @@ const styles = StyleSheet.create({
     height: '100%',
     backgroundColor: '#141726',
   },
-  layout: {
-    //TODO: https://developer.android.com/training/keyboard-input/visibility
-    height: '400%',
-  },
-  innerContainer: {
+  bodyContainer: {
     margin: 30,
     padding: 30,
+    minHeight: '70%',
     backgroundColor: '#21284E',
+    flex: 1,
+    justifyContent: 'space-between',
   },
-  text: {
-    color: '#DBEEF1',
-    marginLeft: 10,
-    marginTop: 5,
-    padding: 5,
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  logoContainer: {
+    position: 'relative',
+    zIndex: 9,
+    marginBottom: 10,
   },
   logo: {
     minHeight: 50,
@@ -63,12 +77,28 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     margin: 10,
   },
-  clockStyles: {
-    height: 70,
-    flexDirection: 'row',
-    marginBottom: 30,
+  h3: {
+    color: '#00D8BB',
+    position: 'absolute',
+    top: 50,
+    left: 80,
   },
   warning: {
     color: '#d7b357',
+  },
+  text: {
+    color: '#9FB7C6',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 12,
+    lineHeight: 15,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    marginBottom: 10,
+  },
+  arrow: {
+    height: 60,
+    width: 60,
+    alignSelf: 'flex-end',
   },
 });
