6cbe5ba65ca7fdf3cd40b57273081efe85a51354
Camille Hall <camille.hall@senseye.co>
2021-04-27 21:55:04 -0500

    updating paths

--- example/src/screens/ResultsScreen.tsx
+++ example/src/screens/ResultsScreen.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { View, Image, StyleSheet, SafeAreaView, Text } from 'react-native';
 import { Spacing, Sizing, Colors, Typography } from '../styles';
-import * as Assets from '../../../src/assets';
+import {
+  ormCheckLogo,
+  warningIcon,
+  checkmarkIcon,
+  xIcon,
+  insufficientIcon,
+} from '@senseyeinc/react-native-senseye-sdk';
 
 export type ResultsScreenProps = {
   /** number to represent FFD */
@@ -14,21 +20,21 @@ export type ResultsScreenProps = {
 export default function ResultsScreen(props: ResultsScreenProps) {
   const resultIcon =
     props.result === 0
-      ? Assets.checkmarkIcon
+      ? checkmarkIcon
       : props.result === 1
-      ? Assets.xIcon
-      : Assets.insufficientIcon;
+      ? xIcon
+      : insufficientIcon;
   return (
     <SafeAreaView style={styles.parentContainer}>
       <View style={styles.childContainer}>
-        <Image style={styles.logo} source={Assets.ormCheckLogo} />
+        <Image style={styles.logo} source={ormCheckLogo} />
         <View style={styles.bodyContainer}>
           <Image style={styles.icon} source={resultIcon} />
           <Text style={styles.text}>{props.message}</Text>
         </View>
       </View>
       <View style={styles.footer}>
-        <Image style={styles.logo} source={Assets.warningIcon} />
+        <Image style={styles.logo} source={warningIcon} />
         <Text style={styles.warning}>{props.disclaimer}</Text>
       </View>
     </SafeAreaView>
