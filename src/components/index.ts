b8678da0480c7a88258531aceb81ce3e862f682d
Camille Hall <camille.hall@senseye.co>
2021-05-07 14:26:20 -0500

    imported svg face box outline

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import VideoRecorder, { VideoRecorderProps } from './VideoRecorder';
 import SenseyeButton, { SenseyeButtonProps } from './SenseyeButton';
 import SenseyePicker, { SenseyePickerProps } from './SenseyePicker';
 import SenseyeTextInput, { SenseyeTextInputProps } from './SenseyeTextInput';
+import FaceOutline, { FaceOutlineProps } from './FaceOutline';
 import * as Tasks from './tasks';
 import * as Surveys from './surveys';
 
@@ -17,6 +18,8 @@ export {
   SenseyePickerProps,
   SenseyeTextInput,
   SenseyeTextInputProps,
+  FaceOutline,
+  FaceOutlineProps,
   Tasks,
   Surveys,
 };
