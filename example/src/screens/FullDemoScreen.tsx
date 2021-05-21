c81529d5e8765e7f35bddc61ccde4554a08ceb3d
Tam Nguyen <tam.nguyen@senseye.co>
2021-05-20 17:31:52 -0700

    added package dependency: react-native-fs

--- example/src/screens/FullDemoScreen.tsx
+++ example/src/screens/FullDemoScreen.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Alert, Modal, Text, View, ViewStyle, TextStyle } from 'react-native';
-// import CameraRoll from '@react-native-community/cameraroll';
 import {
   Constants,
   TaskRunner,
